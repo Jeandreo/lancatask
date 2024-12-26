b4091a51bec59b06d4efa50ad671589578d012b8
SULINKOFICIAL <128751721+SULINKOFICIAL@users.noreply.github.com>
2024-12-26 06:26:10 -0300

    Atualização

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,15 +1,16 @@
-<title>@yield('Page Title', 'Lança Task - O seu sistema de tarefas para lançamentos!') - Lança Task</title>
+<title>@yield('Page Title', 'Lança Task - Gerencie e organize seus lançamentos com eficiência!') - Lança Task</title>
 <meta charset="utf-8" />
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-<meta name="description" content="The most advanced Tailwind CSS & Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. Grab your copy now and get life-time updates for free." />
-<meta name="keywords" content="tailwind, tailwindcss, metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+<meta name="description" content="Lança Task é o sistema ideal para gerenciar e organizar tarefas relacionadas a lançamentos. Simplifique processos, otimize tempo e acompanhe tudo em um só lugar. Experimente agora!" />
+<meta name="keywords" content="gerenciamento de tarefas, lançamentos, organização, produtividade, tarefas, gestão de projetos, software de tarefas, controle de lançamentos, Lança Task, planejamento, time de lançamento" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta property="og:locale" content="en_US" />
-<meta property="og:type" content="article" />
-<meta property="og:title" content="Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes" />
-<meta property="og:url" content="https://keenthemes.com/metronic" />
-<meta property="og:site_name" content="Metronic by Keenthemes" />
-<link rel="canonical" href="http://preview.keenthemes.comindex.html" />
+<meta property="og:locale" content="pt_BR" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Lança Task - Gerencie e organize seus lançamentos com eficiência!" />
+<meta property="og:url" content="https://lancatask.com/" />
+<meta property="og:site_name" content="Lança Task" />
+<meta property="og:description" content="Simplifique o gerenciamento de tarefas para lançamentos com o Lança Task. Acompanhe o progresso do seu time e garanta sucesso nos seus projetos." />
+<link rel="canonical" href="https://lancatask.com/" />
 <link rel="shortcut icon" href="{{ asset('assets/media/images/favicon.png') }}" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
 <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
